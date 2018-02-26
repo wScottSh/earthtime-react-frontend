e89f74f95c7c32e28bd4080220a68fbba28beb3a
wScottSh <7772230+wScottSh@users.noreply.github.com>
2018-02-26 14:52:35 -0600

    got the front end to connect to the backend api!

--- src/Face.js
+++ src/Face.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 import './Face.css';
+import TodoModel from './testModel.js'
 
 class Face extends Component {
   render() {
+    TodoModel.all().then( (res) => {
+      console.log(res);
+    })
     return (
       <h1>Well, hello!</h1>
     );
