930f10be078fdd926329020cf4f064198d53c3d3
wScottSh <7772230+wScottSh@users.noreply.github.com>
2018-03-01 22:54:47 -0600

    got the clock to resize properly on window resizing

--- src/components/ClockFace/ClockFace.js
+++ src/components/ClockFace/ClockFace.js
@@ -3,11 +3,29 @@ import 'react-circular-progressbar/dist/styles.css';
 import './ClockFace.css';
 import CircularProgressbar from 'react-circular-progressbar';
 import axios from 'axios';
-
+import $ from 'jquery';
 
 let fromJSON
-const apiENV = 'https://earthtime-react.herokuapp.com'
-// const apiENV = 'http://localhost:3000'
+// const apiENV = 'https://earthtime-react.herokuapp.com'
+const apiENV = 'http://localhost:3000'
+
+$(function(){
+  $(window).resize(function(){
+    var width  = $(window).width(),
+        height = $(window).height(),
+        padding = .9;
+
+    if (width > height) {
+      $('.clockFace').css('width', height*padding);
+      $('.clockFace').css('height', height*padding);
+    } else {
+      $('.clockFace').css('width', width*padding);
+      $('.clockFace').css('height', width*padding);
+    }
+  })
+  .trigger('resize');
+
+});
 
 class ClockFace extends Component {
   constructor() {
@@ -27,8 +45,7 @@ class ClockFace extends Component {
         let beatCounter = fromJSON.beatLength
         this.timer = setInterval(function(){
           let rightNow = fromJSON.earthTime.now += .01;
-          let dayStart = fromJSON.earthTime.dayStart
-          console.log(dayStart);
+          // let dayStart = fromJSON.earthTime.dayStart
           self.setState({
             now: rightNow,
             fauxPercent: rightNow * .1,
