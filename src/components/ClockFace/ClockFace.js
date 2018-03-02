3efbcc4bc23a5372848e2b1ee9a7c598229d0433
wScottSh <7772230+wScottSh@users.noreply.github.com>
2018-03-01 20:43:24 -0600

    gotz the time to count up correctly

--- src/components/ClockFace/ClockFace.js
+++ src/components/ClockFace/ClockFace.js
@@ -3,58 +3,41 @@ import './ClockFace.css';
 import TimeModel from '../../models/TimeModel.js'
 import axios from 'axios';
 
+let fromJSON
+// const apiENV = 'https://earthtime-react.herokuapp.com'
+const apiENV = 'http://localhost:3000'
+
 class ClockFace extends Component {
   constructor() {
     super();
     this.state = {
-      timeAPI: {
-        "beatLength": 0,
-        "coords": {
-          "lat": 0,
-          "lng": 0
-        },
-        "earthTime": {
-          "now": 0,
-          "dayStart": 0,
-          "solarSight": 0,
-          "solarNoon": 0,
-          "solarClipse": 0,
-          "dayEnd": 0
-        },
-        "unixTime": {
-          "now": 0,
-          "dayStart": 0,
-          "solarSight": 0,
-          "solarNoon": 0,
-          "solarClipse": 0,
-          "dayEnd": 0
-        }
-      }
+      now: 0
     }
   }
 
   componentDidMount() {
     let self = this;
-    axios.get('https://earthtime-react.herokuapp.com/api/v1/earthtime')
+    axios.get(apiENV + '/api/v1/earthtime')
       .then( (res) => {
-        const fromJSON = res.data;
+        fromJSON = res.data;
         console.log(fromJSON);
-        self.setState({
-          timeAPI: fromJSON
-        })
-        console.log(this.state.timeAPI.earthTime.now);
+
+        let beatCounter = fromJSON.beatLength
+        this.timer = setInterval(function(){
+          let rightNow = fromJSON.earthTime.now += .01;
+          // console.log(rightNow);
+          self.setState({
+            now: rightNow
+          })
+        }, beatCounter/100)
       })
   }
 
-  incrementTimer = () => {
-    let beatCounter = this.state.timeAPI.beatLength
-    console.log(beatCounter);
-  }
-
   render() {
+    let roundedBeat = Number(Math.round(this.state.now+'e1')+'e-1');
     return (
       <div className="clockFace">
-        <h1>{this.state.timeAPI.earthTime.now}</h1>
+        <h1>{roundedBeat}</h1>
         <div className="spinnyBox"></div>
       </div>
     );
